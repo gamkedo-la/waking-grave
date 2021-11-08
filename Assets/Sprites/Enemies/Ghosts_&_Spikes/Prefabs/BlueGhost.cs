8c954942614af858dec5dadbd5e238073b771501
Armando Navarrete <viking805fan@yahoo.com>
2021-11-08 12:50:17 -0800

    Ghosts shoot a projectile that tracks the player in order to test the Fireball. W.I.P.

---
+++ Assets/Sprites/Enemies/Ghosts_&_Spikes/Prefabs/BlueGhost.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueGhost : MonoBehaviour {
+
+    [SerializeField]
+    GameObject Fireball;
+
+    float fireRate;
+    float nextFire;
+
+    // Use this for initialization
+    void Start() {
+        fireRate = 3f;
+        nextFire = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckIfTimeToFire();
+
+    }
+
+    void CheckIfTimeToFire()
+    {
+        if (Time.time > nextFire)
+        {
+            Instantiate(Fireball, transform.position, Quaternion.identity);
+            nextFire = Time.time + fireRate;
+        }
+    }
+}
---
+++ Assets/Sprites/Fireball/Prefab/Fireball.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour {
+
+    float moveSpeed = 5f;
+
+    Rigidbody2D rb;
+
+    PlayerController target;
+    Vector2 moveDirection;
+
+    // Use this for initialization
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody2D> ();
+        target = GameObject.FindObjectOfType<PlayerController>();
+        moveDirection = (target.transform.position - transform.position).normalized * moveSpeed;
+        rb.velocity = new Vector2 (moveDirection.x, moveDirection.y);
+        Destroy (gameObject, 3f);
+    }
+
+    void OnTriggerEnter2D (Collider2D col)
+    {
+        if (col.gameObject.name.Equals ("PlayerController"))
+        {
+            Debug.Log("Hit!");
+            Destroy (gameObject);
+        }
+    }
+
+   
+}
