8ea76303d5ce2c1aeb2bbd4ec3f3d8502fe536de
Christer Kaitila <breakdance@mcfunkypants.com>
2021-11-09 13:44:52 -0800

    zombies can die and spawn zombie ragdolls

--- Assets/Scripts/Enemies/FlingAndDie.cs
+++ Assets/Scripts/Enemies/FlingAndDie.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class FlingAndDie : MonoBehaviour
 {
+    [Header("Explosion Forces!! (influenced by mass)")]
+    public float forceXmin = -10;
+    public float forceXmax = 10;
+    public float forceYmin = 1;
+    public float forceYmax = 10;
+    public float spinMin = -10;
+    public float spinMax = 10;
+
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(-100f,100f),Random.Range(1f,100f)));
-        GetComponent<Rigidbody2D>().AddTorque(Random.Range(-100f,100f));
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(forceXmin,forceXmax),Random.Range(forceYmin,forceYmax)));
+        GetComponent<Rigidbody2D>().AddTorque(Random.Range(spinMin,spinMax));
         Destroy(gameObject,5f);
     }
 
