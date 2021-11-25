0d71a33433e17a3f77a80af66e486b8d92ce1215
stebssbets <stebssbets@yahoo.com>
2021-11-25 12:39:34 +0400

    warlocks retreat position randomizes

---
+++ Assets/RetreatPositionScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RetreatPositionScript : MonoBehaviour
+{
+    [SerializeField] GameObject leftBoundGameObject;
+    [SerializeField] GameObject rightBoundGameObject;
+    [SerializeField] GameObject lowerBoundGameObject;
+    [SerializeField] GameObject upperBoundGameObject;
+
+    private float leftBoundX;
+    private float rightBoundX;
+    private float upperBoundY;
+    private float lowerBoundY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftBoundX = leftBoundGameObject.transform.position.x;
+        rightBoundX = rightBoundGameObject.transform.position.x;
+        upperBoundY = upperBoundGameObject.transform.position.y;
+        lowerBoundY = lowerBoundGameObject.transform.position.y;
+        ResetPosition();
+    }
+
+    public void ResetPosition()
+    {
+        float newX = Random.Range(leftBoundX, rightBoundX);
+        float newY = Random.Range(lowerBoundY, upperBoundY);
+        Debug.Log("newX: " + newX);
+
+        Vector2 newPosition = new Vector2(newX, newY);
+        gameObject.transform.position = newPosition;
+    }
+}
--- Assets/Scripts/Enemies/MasterWarlock/WarlockRetreating.cs
+++ Assets/Scripts/Enemies/MasterWarlock/WarlockRetreating.cs
@@ -6,7 +6,6 @@ public class WarlockRetreating : BaseState
 {
     WarlockSM _sm;
     float speed = 0.125f;
-    
 
     [SerializeField] GameObject warlocksRetreatPosition;
     public WarlockRetreating(WarlockSM stateMachine) : base("WarlockRetreating", stateMachine)
@@ -17,22 +16,25 @@ public class WarlockRetreating : BaseState
     public override void Enter()
     {
         base.Enter();
+        warlocksRetreatPosition = GameObject.FindGameObjectWithTag("WarlockRetreatPosition");
     }
 
     public override void UpdateLogic()
     {
         base.UpdateLogic();
 
-        warlocksRetreatPosition = GameObject.FindGameObjectWithTag("WarlockRetreatPosition");
+        Debug.Log("warlocksRetreatPosition.transform.position.x: " + warlocksRetreatPosition.transform.position.x);
 
-        if (_sm.transform.position != warlocksRetreatPosition.transform.position)
+        if (_sm.transform.position.x != warlocksRetreatPosition.transform.position.x)
         {
             _sm.transform.position = Vector2.MoveTowards(_sm.transform.position, warlocksRetreatPosition.transform.position, speed);
-        }
+        }    
         else if (_sm.transform.position.x - warlocksRetreatPosition.transform.position.x < 0.1 &&
             _sm.transform.position.y - warlocksRetreatPosition.transform.position.y < 0.1)
         {
             _sm.ChangeState(_sm.idleState);
+            Debug.Log("warlocksRetreatPosition: " + warlocksRetreatPosition);
+            warlocksRetreatPosition.GetComponent<RetreatPositionScript>().ResetPosition();
         }
     }
 }
