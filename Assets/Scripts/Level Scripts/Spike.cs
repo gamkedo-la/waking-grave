4098cb288926633f27db0e86309b93ce263dcb0e
Luis Montaña <lmontana@live.com>
2021-12-14 12:36:27 -0500

    Added spike sprite with animation and created initial spike script

---
+++ Assets/Scripts/Level Scripts/Spike.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    Animator anim;
+    public float direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        direction = transform.localScale.x;
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Player")){
+            anim.Play("Activate");
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.CompareTag("Player")){
+            // Player Pushback function
+        }
+    }
+}
