d927bcef477ea0da099b6176623b4651c33e355f
Luis Montaña <lmontana@live.com>
2021-12-19 10:05:30 -0500

    Added Cheats to move fast around level. Added UI sorting layer

---
+++ Assets/Scripts/Cheats.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cheats : MonoBehaviour
+{
+    public Transform cam;
+    public Transform player;
+    public void MoveToBossGraveyard() {
+        cam.position = new Vector3 (250.5f, -9, -10);
+        cam.GetComponent<CameraFollow>().enabled = false;
+        player.position = new Vector3(240, -14.23f, 0);
+    }
+
+    public void MoveToStartGraveyard() {
+        cam.position = new Vector3 (0.6f, 0.7f, -10);
+        cam.GetComponent<CameraFollow>().enabled = true;
+        player.position = new Vector3(0.6f, -2.2f, 0);
+    }
+}
