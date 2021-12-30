2f0dabc7eddccc3b4b09462dc4dba074eca514f6
Luis Montaña <lmontana@live.com>
2021-12-30 10:58:29 -0500

    Added cam setup when player arrives to boss

--- Assets/Scripts/BossSpawner.cs
+++ Assets/Scripts/BossSpawner.cs
@@ -8,13 +8,32 @@ public class BossSpawner : MonoBehaviour
     public GameObject stageWall;
     public GameObject bossHealthBar;
     public GameObject cam;
+    public float camSpeed;
+    private bool moveCam;
+    private Vector3 camTarget = new Vector3 (250.5f, -9, -10);
+
+    private void FixedUpdate() {
+        if(moveCam) {
+            cam.transform.position = Vector3.MoveTowards(cam.transform.position, camTarget , camSpeed * Time.deltaTime);
+            if(Vector3.Distance(cam.transform.position, camTarget) == 0) {
+                EnableBoss();
+                moveCam = false;
+            }
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("Player")) {
-            abomination.SetActive(true);
             stageWall.SetActive(true);
-            bossHealthBar.SetActive(true);
-            gameObject.SetActive(false);
             cam.GetComponent<CameraFollow>().enabled = false;
+            moveCam = true;
+            GetComponent<BoxCollider2D>().enabled = false;
         }
+    }
+
+    private void EnableBoss () {
+        bossHealthBar.SetActive(true);
+        abomination.SetActive(true);
+
     }
 }
